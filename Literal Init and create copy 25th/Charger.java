c7f912257ad6f5ead4781512202b9366d23d5056
Nirmalap34 <nirmalap.xworkz@gmail.com>
2024-07-30 10:30:44 +0530

    literals

---
+++ Literal Init and create copy 25th/Charger.java
@@ -0,0 +1,10 @@
+class Charger
+{
+	String company;
+	double price=200;
+	int quantity;
+	 Charger()
+	 {
+		 System.out.println("Created a Charger");
+	 }
+}
---
+++ Literal Init and create copy 25th/Claw.java
@@ -0,0 +1,11 @@
+class Claw
+{
+	String clawUse;
+	String clawColor;
+	int noOfClaw=4;
+	
+	Claw()
+	{
+		System.out.println(" created a Claw");
+	}
+}
